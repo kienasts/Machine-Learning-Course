fb16f0b1bf1c7863b1abe79b000e2b06644441f4
kienasts <simon.kienast@unibas.ch>
2026-03-14 10:22:45 +0100

    data cleaning

--- Group Data Challenge 2026/scripts/group-assignment.R
+++ Group Data Challenge 2026/scripts/group-assignment.R
@@ -18,13 +18,35 @@ summary(wage_training)
 colMeans(is.na(wage_training))
 glimpse(wage_training)
 
-var_numeric <- wage_training %>%
-  select_if(is.numeric) %>%
-  names()
+var_num <- c("yos", "exp", "exp2", "age", "famsize", "nchlt5", "hoursworked", "lwwage")
+var_cat <- c("sex", "marst", "race", "speakeng", "hispanic", "schltype", "degfield", "vetstat", "educd",
+             "education", "ind1990c", "occ2010c", "region", "dchlt5", "dchlt19", "msa", "metro", "hw")
 
-var_categorical <- wage_training %>%
-  select_if(is.character) %>%
-  names()
+check <- tibble(
+  variable = names(wage_training),
+  actual_class = sapply(wage_training, class)
+) %>%
+  mutate(
+    expected = case_when(
+      variable %in% var_num ~ "numeric",
+      variable %in% var_cat ~ "factor/character",
+      TRUE ~ "unlisted / check me"
+    ),
+    mismatch = case_when(
+      variable %in% var_num & !actual_class %in% c("numeric", "integer") ~ TRUE,
+      variable %in% var_cat & !actual_class %in% c("factor", "character") ~ TRUE,
+      TRUE ~ FALSE
+    )
+  )
+
+check %>% 
+  print(n = Inf)
+
+
+
+
+
+
 
 wage_training %>% 
   select(all_of(var_numeric)) %>%
